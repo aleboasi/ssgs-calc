75edfc367759acdf959445d424259bd6c64caaaa
Alessia Boasi <4384458@studenti.unige.it>
2025-04-05 12:51:25 +0200

    Update operation.test.js

--- operation.test.js
+++ operation.test.js
@@ -4,6 +4,6 @@ test('Somma di 2 e 2 deve essere 4', () => {
     expect(add(2, 2)).toBe(4);
 });
 
-test('Somma di 3 e 5 deve essere 7', () => {
-    expect(add(3, 5)).toBe(7);
+test('Somma di 3 e 5 deve essere 8', () => {
+    expect(add(3, 5)).toBe(8);
 });
